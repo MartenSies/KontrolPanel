56c45b1470acfff6933e2db54f8936b8beacb63a
Marten Sies <mail@martensies.nl>
2019-10-21 20:37:17 +0200

    Added backend workflow

--- backend_svc/setup.py
+++ backend_svc/setup.py
@@ -1,5 +1,8 @@
-import os
 
+import os
+import subprocess
+
+from distutils.core import Command
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -16,33 +19,40 @@ requires = [
     'kubernetes',
 ]
 
-tests_require = [
+test_requires = [
     'WebTest >= 1.3.1',  # py3 compat
     'pytest >= 3.7.4',
     'pytest-cov',
 ]
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        subprocess.call(['pip', 'install'] + test_requires)  # nosec
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import pytest
+        errno = pytest.main(['--cov-report', 'term-missing:skip-covered',
+                             '--cov-report', 'xml',
+                             '--cov', 'backend_svc',
+                             '--cov', 'test',
+                             'test'])
+
+        raise SystemExit(errno)
+
 
 setup(
     name='backend_svc',
     version='0.0',
     description='backend-svc',
     long_description=README + '\n\n' + CHANGES,
-    classifiers=[
-        'Programming Language :: Python',
-        'Framework :: Pyramid',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
-    ],
-    author='',
-    author_email='',
-    url='',
-    keywords='web pyramid pylons',
+    cmdclass={'test': PyTest},
     packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    extras_require={
-        'testing': tests_require,
-    },
+    tests_require=test_requires,
     install_requires=requires,
     entry_points={
         'paste.app_factory': [
